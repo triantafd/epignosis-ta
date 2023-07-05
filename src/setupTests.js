c0206311dd8b9ba2b6f768fa8c599146567a3afc
dimitris <triantafd@iti.gr>
2023-07-05 18:53:22 +0300

    cmmend msw server, (unused)

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,22 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { server } from './mocks/servers'
+/* import { server } from './mocks/servers'
 
 beforeAll(() => {
-  // Establish requests interception layer before all tests.
   server.listen()
 })
 
 afterEach(() => {
-  // Clean up after all tests are done, preventing this
-  // interception layer from affecting irrelevant tests.
   server.resetHandlers()
 })
 
 
 afterAll(() => {
-  // Clean up after all tests are done, preventing this
-  // interception layer from affecting irrelevant tests.
   server.close()
-})
+}) */
